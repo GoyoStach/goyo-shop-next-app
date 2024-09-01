141fc1f6457b0afb36d07ab36d63652a26997bdc
GoyoStach <Guillaume.excoffier86@gmail.com>
2024-09-01 16:13:25 +0200

    Adding font params

--- src/app/styles/font.ts
+++ src/app/styles/font.ts
@@ -1,14 +1,18 @@
 // app/fonts.ts
-import { Silkscreen, M_PLUS_1p } from 'next/font/google';
+import { M_PLUS_1p, Silkscreen } from 'next/font/google';
 
 const silkscreen = Silkscreen({
   subsets: ['latin'],
   variable: '--font-silkscreen',
   weight: ['400', '700'],
+  display: 'swap',
+  adjustFontFallback: false,
 });
 
 const mplus = M_PLUS_1p({
   subsets: ['latin'],
+  display: 'swap',
+  adjustFontFallback: false,
   variable: '--font-mplus',
   weight: ['100', '300', '400', '500', '700', '800', '900'],
 });
